d7311fceed2c5b134534ebe5ebb7c0b5239321dc
Ernie Massei <Emassei05@gmail.com>
2014-05-22 12:05:00 -0700

    Created a new interactive Buzz Fizz Exercise!

---
+++ javaScript/BuzzFizz/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$('form').submit(function(event){
+		event.preventDefault();
+
+		var inputtedBuzz = $('#buzzfizz-input').val();
+		parseInt(inputtedBuzz);
+
+		var buzzFizzCalc = function(){
+			for(i=1;i<=inputtedBuzz;i++){
+			if(i%3===0 && i%5===0){
+				$('ul#list').append('<li>' + "BuzzFizz" + '</li>');
+				}
+			else if(i%3===0){
+				$('ul#list').append('<li>' + 'Buzz' + '</li>');
+				}
+			else if(i%5===0){
+				$('ul#list').append('<li>' + 'Fizz' + '</li>');
+			}
+			else{
+				$('ul#list').append('<li>'+ i + '</li>');
+			}
+			}
+		}
+		buzzFizzCalc();
+	});
+});
